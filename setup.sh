e85a67f34ee17a75ad4dacf675327b5f8d95461a
xh-dev <adamhung12@gmail.com>
2024-04-05 01:08:52 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-basePath=$(dirname $0)
+# This script no need to be aware of the basePath
+#basePath=$(dirname $0)
 
+echo "remove existing .vimrc"
 rm -f ~/.vimrc
+
+echo "clone repo"
 git clone git@github.com:xh-dev/my-vimrc.git
-cp $basePath/my-vimrc/.vimrc ~/.vimrc
+
+echo "copy .vimrc"
+cp my-vimrc/.vimrc ~/.vimrc
+
+echo "remove the cloned repo"
 rm -fr my-vimrc
